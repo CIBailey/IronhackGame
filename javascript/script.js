541b05a22ad2aa5b0df0ea685894f77a9049920e
Christina Bailey <christinaibailey@live.com>
2019-01-21 17:46:07 +0100

    Finished CSS and now starting Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+//Select Body and Click
+var start = document.querySelector("body");
+start.addEventListener("click", startFunction);
+// Event on click
+function startFunction() {
+  console.log("click");
+  var popup = document.querySelector(".popup-welcome");
+  popup.style.visibility = "hidden";
+}
+
+////////////////////////// Dropping Items
+var canvas = document.querySelector(".my-game");
+var ctx = canvas.getContext("2d");
+
+var Movable = function() {};
+
+var Movable = function() {};
+
+var Score = function() {};
+
+var Pastry = function() {};
+
+var Basket = function() {};
